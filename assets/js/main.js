63c3bfb07c35c260e438f7a4e599e89739611611
Omar Nassar <omar.portero.nassar1@gmail.com>
2024-06-22 15:35:36 +0200

    images and more

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,8 +16,9 @@ const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove("show");
+  if (navMenu) {
+    navMenu.classList.remove("show");
+  }
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
@@ -35,10 +36,12 @@ const scrollActive = () => {
         ".nav__menu a[href*=" + sectionId + "]"
       );
 
-    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-      sectionsClass.classList.add("active-link");
-    } else {
-      sectionsClass.classList.remove("active-link");
+    if (sectionsClass) {
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionsClass.classList.add("active-link");
+      } else {
+        sectionsClass.classList.remove("active-link");
+      }
     }
   });
 };
@@ -60,35 +63,37 @@ sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
 sr.reveal(".home__social-icon", { interval: 200 });
 sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
 
-//contact orm
+//contact form
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector(".contact__form");
-  form.addEventListener("submit", async function (e) {
-    e.preventDefault();
-    const formData = new FormData(form);
-    try {
-      const response = await fetch(form.action, {
-        method: form.method,
-        body: formData,
-        headers: {
-          Accept: "application/json",
-        },
-      });
-      if (response.ok) {
-        form.reset();
-        alert("Thank you for your message. We will get back to you soon.");
-      } else {
+  if (form) {
+    form.addEventListener("submit", async function (e) {
+      e.preventDefault();
+      const formData = new FormData(form);
+      try {
+        const response = await fetch(form.action, {
+          method: form.method,
+          body: formData,
+          headers: {
+            Accept: "application/json",
+          },
+        });
+        if (response.ok) {
+          form.reset();
+          alert("Thank you for your message. We will get back to you soon.");
+        } else {
+          alert("Oops! There was a problem submitting your form.");
+        }
+      } catch (error) {
         alert("Oops! There was a problem submitting your form.");
       }
-    } catch (error) {
-      alert("Oops! There was a problem submitting your form.");
-    }
-  });
+    });
+  }
 });
 
 //thumbnail services:
 document.addEventListener("DOMContentLoaded", () => {
-  const thumbnails = document.querySelectorAll(".services__thumbnail");
+  const clickableImages = document.querySelectorAll(".clickable");
   const lightbox = document.createElement("div");
   lightbox.classList.add("lightbox");
   document.body.appendChild(lightbox);
@@ -96,14 +101,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const img = document.createElement("img");
   lightbox.appendChild(img);
 
-  thumbnails.forEach((thumbnail) => {
-    thumbnail.addEventListener("click", () => {
-      img.src = thumbnail.src;
-      lightbox.classList.add("visible");
-    });
+  clickableImages.forEach((image) => {
+      image.addEventListener("click", () => {
+          img.src = image.src;
+          lightbox.classList.add("visible");
+      });
   });
 
   lightbox.addEventListener("click", () => {
-    lightbox.classList.remove("visible");
+      lightbox.classList.remove("visible");
   });
 });
